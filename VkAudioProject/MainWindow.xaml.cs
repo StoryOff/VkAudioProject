e5223a68535b0515f494ae976db680b16ac51ece
StoryOff <sotic.roma@gmail.com>
2020-12-26 16:58:44 +0800

    no message

--- VkAudioProject/MainWindow.xaml.cs
+++ VkAudioProject/MainWindow.xaml.cs
@@ -1,28 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Interop;
+using MahApps.Metro.Controls;
+using VkAudioProject.ViewModels;
 
 namespace VkAudioProject
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainViewModel();
         }
     }
 }
---
+++ VkAudioProject/ViewModels/Settings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VkAudioProject.ViewModels
+{
+    class Settings : JsonSettings
+    {
+        #region Storage
+        private static Settings _instance;
+        private string _token;
+        #endregion
+
+        public static Settings Instance
+        {
+            get => _instance ??= new Settings();
+        }
+
+        public string Token
+        {
+            get => _token;
+            set => SetProperty(ref _token, value);
+        }
+    }
+}
